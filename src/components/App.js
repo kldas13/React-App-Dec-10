f638cfa0d5b0feae23aa0f69dc0c1b930153f11f
KLDas <19etmm01@uohyd.ac.in>
2022-12-10 13:33:38 +0530

    Added ReactApp

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+import Render from "./Render.jsx";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Render id="root"/>
+    </div>
+  )
+}
+
+
+export default App;
---
+++ src/components/Render.jsx
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import '../styles/Render.css';
+
+const Render = () => {
+  return (
+    <div id="main">
+      <img src="https://ionicframework.com/docs/icons/logo-react-icon.png" alt="react" />
+      <p id="root">I am learning React. My life is getting better.</p>
+    </div>
+  )
+}
+
+
+export default Render;
